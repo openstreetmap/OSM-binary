282bb229f502985f68e1c7f3552b264db4193a7d
Jochen Topf <jochen@topf.org>
2011-06-03 12:05:44 +0200

    New osmpbf.h and new debian package.
    * New osmpbf.h makes including easier and defines some often used constants
    * README now explains how to use the lib
    * New version of debian package includes
        * osmpbf.h
        * osmpbf-outline tool
        * README

--- src/osmpbf.h
+++ src/osmpbf.h
@@ -7,13 +7,20 @@
 // this describes the high-level OSM objects
 #include <osmpbf/osmformat.pb.h>
 
-namespace osmpbf {
+namespace OSMPBF {
+
+    // library version
+    const char *version = "1.1.1j1";
 
     // the maximum size of a blob header in bytes
-    const int max_blob_header_size = 64 * 1024;
+    const int max_blob_header_size = 64 * 1024; // 64 kB
 
-    // the maximum size of a blob in bytes
-    const int max_uncompressed_blob_size = 32 * 1024 * 1024;
+    // the maximum size of an uncompressed blob in bytes
+    const int max_uncompressed_blob_size = 32 * 1024 * 1024; // 32 MB
+
+    // resolution for longitude/latitude used for conversion
+    // between representation as double and as int
+    const int lonlat_resolution = 1000 * 1000 * 1000; 
 
 }
 
